4901b18ad795655727078d468f6ad9cee3634e03
swiftlee <jconlin@ufl.edu>
2021-02-15 02:02:59 -0500

    injected into handleJoinGame for lobby switching in hypixel (bedwars)
    Co-authored-by: mblakew <michaelbwilliams100@gmail.com>

--- src/launch/java/baritone/launch/mixins/MixinClientPlayNetHandler.java
+++ src/launch/java/baritone/launch/mixins/MixinClientPlayNetHandler.java
@@ -22,11 +22,26 @@ import baritone.api.BaritoneAPI;
 import baritone.api.IBaritone;
 import baritone.api.event.events.ChunkEvent;
 import baritone.api.event.events.type.EventState;
+import baritone.api.utils.Helper;
+import baritone.api.utils.IPlayerContext;
+import baritone.api.utils.IPlayerController;
 import baritone.cache.CachedChunk;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.network.play.ClientPlayNetHandler;
+import net.minecraft.entity.Entity;
+import net.minecraft.inventory.container.ChestContainer;
+import net.minecraft.inventory.container.ClickType;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.container.Slot;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.server.*;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.World;
+
+import com.mojang.realmsclient.gui.RealmsServerSlotButton.Action;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -37,86 +52,124 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  * @since 8/3/2018
  */
 @Mixin(ClientPlayNetHandler.class)
-public class MixinClientPlayNetHandler {
+public class MixinClientPlayNetHandler implements Helper {
 
     // unused lol
-    /*@Inject(
-            method = "handleChunkData",
-            at = @At(
-                    value = "INVOKE",
-                    target = "net/minecraft/client/multiplayer/ChunkProviderClient.func_212474_a(IILnet/minecraft/network/PacketBuffer;IZ)Lnet/minecraft/world/chunk/Chunk;"
-            )
-    )
-    private void preRead(SPacketChunkData packetIn, CallbackInfo ci) {
-        for (IBaritone ibaritone : BaritoneAPI.getProvider().getAllBaritones()) {
-            ClientPlayerEntity player = ibaritone.getPlayerContext().player();
-            if (player != null && player.connection == (ClientPlayNetHandler) (Object) this) {
-                ibaritone.getGameEventHandler().onChunkEvent(
-                        new ChunkEvent(
-                                EventState.PRE,
-                                packetIn.isFullChunk() ? ChunkEvent.Type.POPULATE_FULL : ChunkEvent.Type.POPULATE_PARTIAL,
-                                packetIn.getChunkX(),
-                                packetIn.getChunkZ()
-                        )
-                );
-            }
-        }
-    }*/
+    /*
+     * @Inject( method = "handleChunkData", at = @At( value = "INVOKE", target =
+     * "net/minecraft/client/multiplayer/ChunkProviderClient.func_212474_a(IILnet/minecraft/network/PacketBuffer;IZ)Lnet/minecraft/world/chunk/Chunk;"
+     * ) ) private void preRead(SPacketChunkData packetIn, CallbackInfo ci) { for
+     * (IBaritone ibaritone : BaritoneAPI.getProvider().getAllBaritones()) {
+     * ClientPlayerEntity player = ibaritone.getPlayerContext().player(); if (player
+     * != null && player.connection == (ClientPlayNetHandler) (Object) this) {
+     * ibaritone.getGameEventHandler().onChunkEvent( new ChunkEvent( EventState.PRE,
+     * packetIn.isFullChunk() ? ChunkEvent.Type.POPULATE_FULL :
+     * ChunkEvent.Type.POPULATE_PARTIAL, packetIn.getChunkX(), packetIn.getChunkZ()
+     * ) ); } } }
+     */
 
-    @Inject(
-            method = "handleChunkData",
-            at = @At("RETURN")
-    )
+    @Inject(method = "handleJoinGame", at = @At("RETURN"))
+    private void doPathOnClientJoinGame(SJoinGamePacket packetIn, CallbackInfo ci) {
+        String prefix = "\u00A77\u00A7l[\u00A7e\u00A7l!\u00A77\u00A7l]\u00A7r ";
+        logDirect("Bri'ish are ya? Ow'd ya know?");
+        IBaritone baritone = BaritoneAPI.getProvider().getPrimaryBaritone();
+        IPlayerContext primaryPlayerContext = baritone.getPlayerContext();
+        ClientPlayerEntity player = primaryPlayerContext.player();
+        if (player != null) {
+            logDirect("Player is not null");
+            World world = player.world;
+            Entity entity = world.getEntityByID(packetIn.getPlayerId());
+            if (entity != null && entity.getUniqueID().equals(player.getUniqueID())) {
+                logDirect("Entity is not null");
+                logDirect(prefix + "\u00A7a\u00A7l\u00A7uIDs matched!");
+                IPlayerController playerController = primaryPlayerContext.playerController();
+                if (playerController != null) {
+                    ActionResultType result = playerController.processRightClick(player, world, Hand.MAIN_HAND);
+
+                    if (result == ActionResultType.SUCCESS) {
+                        logDirect("success");
+                    } else {
+                        logDirect("fail");
+                    }
+
+                    Thread t = new Thread(() -> {
+                        // TODO: why tho
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                        Container gui = player.openContainer;
+                        /*
+                         * while (gui == player.container) { gui =
+                         * BaritoneAPI.getProvider().getPrimaryBaritone().getPlayerContext()
+                         * .player().openContainer; }
+                         */
+                        if (gui != null) {
+                            int slotId = 13;
+                            ItemStack s = playerController.windowClick(player.openContainer.windowId, slotId, 1,
+                                    ClickType.PICKUP, player);
+                            logDirect("Item stack: " + s.getDisplayName());
+                        } else {
+                            logDirect("I plead Oopsie-Daisy Your Honor");
+                        }
+                    });
+
+                    t.start();
+
+                }
+
+                // baritone.getInputOverrideHandler().clearAllKeys();
+                // baritone.getInputOverrideHandler().setInputForceState(Input.CLICK_RIGHT,
+                // true);
+                logDirect("After trying to click!");
+            }
+            // FOR REFERENCE LATER WHEN CLICKING INVENTORY
+            // baritone.getPlayerContext().playerController().windowClick(windowId,
+            // slotId,
+            // mouseButton, type, player)
+        }
+
+    }
+
+    @Inject(method = "handleChunkData", at = @At("RETURN"))
     private void postHandleChunkData(SChunkDataPacket packetIn, CallbackInfo ci) {
         for (IBaritone ibaritone : BaritoneAPI.getProvider().getAllBaritones()) {
             ClientPlayerEntity player = ibaritone.getPlayerContext().player();
             if (player != null && player.connection == (ClientPlayNetHandler) (Object) this) {
-                ibaritone.getGameEventHandler().onChunkEvent(
-                        new ChunkEvent(
-                                EventState.POST,
-                                packetIn.isFullChunk() ? ChunkEvent.Type.POPULATE_FULL : ChunkEvent.Type.POPULATE_PARTIAL,
-                                packetIn.getChunkX(),
-                                packetIn.getChunkZ()
-                        )
-                );
+                ibaritone.getGameEventHandler()
+                        .onChunkEvent(new ChunkEvent(EventState.POST,
+                                packetIn.isFullChunk() ? ChunkEvent.Type.POPULATE_FULL
+                                        : ChunkEvent.Type.POPULATE_PARTIAL,
+                                packetIn.getChunkX(), packetIn.getChunkZ()));
             }
         }
     }
 
-    @Inject(
-            method = "processChunkUnload",
-            at = @At("HEAD")
-    )
+    @Inject(method = "processChunkUnload", at = @At("HEAD"))
     private void preChunkUnload(SUnloadChunkPacket packet, CallbackInfo ci) {
         for (IBaritone ibaritone : BaritoneAPI.getProvider().getAllBaritones()) {
             ClientPlayerEntity player = ibaritone.getPlayerContext().player();
             if (player != null && player.connection == (ClientPlayNetHandler) (Object) this) {
                 ibaritone.getGameEventHandler().onChunkEvent(
-                        new ChunkEvent(EventState.PRE, ChunkEvent.Type.UNLOAD, packet.getX(), packet.getZ())
-                );
+                        new ChunkEvent(EventState.PRE, ChunkEvent.Type.UNLOAD, packet.getX(), packet.getZ()));
             }
         }
     }
 
-    @Inject(
-            method = "processChunkUnload",
-            at = @At("RETURN")
-    )
+    @Inject(method = "processChunkUnload", at = @At("RETURN"))
     private void postChunkUnload(SUnloadChunkPacket packet, CallbackInfo ci) {
         for (IBaritone ibaritone : BaritoneAPI.getProvider().getAllBaritones()) {
             ClientPlayerEntity player = ibaritone.getPlayerContext().player();
             if (player != null && player.connection == (ClientPlayNetHandler) (Object) this) {
                 ibaritone.getGameEventHandler().onChunkEvent(
-                        new ChunkEvent(EventState.POST, ChunkEvent.Type.UNLOAD, packet.getX(), packet.getZ())
-                );
+                        new ChunkEvent(EventState.POST, ChunkEvent.Type.UNLOAD, packet.getX(), packet.getZ()));
             }
         }
     }
 
-    @Inject(
-            method = "handleBlockChange",
-            at = @At("RETURN")
-    )
+    @Inject(method = "handleBlockChange", at = @At("RETURN"))
     private void postHandleBlockChange(SChangeBlockPacket packetIn, CallbackInfo ci) {
         if (!Baritone.settings().repackOnAnyBlockChange.value) {
             return;
@@ -127,22 +180,13 @@ public class MixinClientPlayNetHandler {
         for (IBaritone ibaritone : BaritoneAPI.getProvider().getAllBaritones()) {
             ClientPlayerEntity player = ibaritone.getPlayerContext().player();
             if (player != null && player.connection == (ClientPlayNetHandler) (Object) this) {
-                ibaritone.getGameEventHandler().onChunkEvent(
-                        new ChunkEvent(
-                                EventState.POST,
-                                ChunkEvent.Type.POPULATE_FULL,
-                                packetIn.getPos().getX() >> 4,
-                                packetIn.getPos().getZ() >> 4
-                        )
-                );
+                ibaritone.getGameEventHandler().onChunkEvent(new ChunkEvent(EventState.POST,
+                        ChunkEvent.Type.POPULATE_FULL, packetIn.getPos().getX() >> 4, packetIn.getPos().getZ() >> 4));
             }
         }
     }
 
-    @Inject(
-            method = "handleMultiBlockChange",
-            at = @At("RETURN")
-    )
+    @Inject(method = "handleMultiBlockChange", at = @At("RETURN"))
     private void postHandleMultiBlockChange(SMultiBlockChangePacket packetIn, CallbackInfo ci) {
         if (!Baritone.settings().repackOnAnyBlockChange.value) {
             return;
@@ -160,24 +204,12 @@ public class MixinClientPlayNetHandler {
             ClientPlayerEntity player = ibaritone.getPlayerContext().player();
             if (player != null && player.connection == (ClientPlayNetHandler) (Object) this) {
                 ibaritone.getGameEventHandler().onChunkEvent(
-                        new ChunkEvent(
-                                EventState.POST,
-                                ChunkEvent.Type.POPULATE_FULL,
-                                chunkPos[0].x,
-                                chunkPos[0].z
-                        )
-                );
+                        new ChunkEvent(EventState.POST, ChunkEvent.Type.POPULATE_FULL, chunkPos[0].x, chunkPos[0].z));
             }
         }
     }
 
-    @Inject(
-            method = "handleCombatEvent",
-            at = @At(
-                    value = "INVOKE",
-                    target = "net/minecraft/client/Minecraft.displayGuiScreen(Lnet/minecraft/client/gui/screen/Screen;)V"
-            )
-    )
+    @Inject(method = "handleCombatEvent", at = @At(value = "INVOKE", target = "net/minecraft/client/Minecraft.displayGuiScreen(Lnet/minecraft/client/gui/screen/Screen;)V"))
     private void onPlayerDeath(SCombatPacket packetIn, CallbackInfo ci) {
         for (IBaritone ibaritone : BaritoneAPI.getProvider().getAllBaritones()) {
             ClientPlayerEntity player = ibaritone.getPlayerContext().player();
--- src/launch/java/baritone/launch/mixins/MixinClientWorld.java
+++ src/launch/java/baritone/launch/mixins/MixinClientWorld.java
@@ -1,5 +1,7 @@
 package baritone.launch.mixins;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -31,7 +33,7 @@ import net.minecraft.util.text.TextFormatting;
 @Mixin(ClientWorld.class)
 public class MixinClientWorld implements Helper {
     // int playerId, AbstractClientPlayerEntity playerEntityIn
-    @Inject(method = "addPlayer", at = @At("RETURN"))
+   /* @Inject(method = "addPlayer", at = @At("RETURN"))
     public void doPathOnPlayerSpawn(int playerID, AbstractClientPlayerEntity playerEntityIn, CallbackInfo ci) {
         // TODO:
         // Master receives information from other players on join
@@ -49,68 +51,74 @@ public class MixinClientWorld implements Helper {
         // ISSUE: inconsistent opening of game menu, setting item to 0 is working though
         // Still need to figure out how to click bed
 
-        /*
+        *//*
          * ExecutorService executor = Executors.newSingleThreadExecutor(); class
          * MoveTask implements Runnable, Helper {
-         * 
+         *
          * @Override public void run() { try {
-         */
+         *//*
         // if (world != null) {
         IBaritone baritone = BaritoneAPI.getProvider().getPrimaryBaritone();
         // baritone.getPlayerContext();
         ClientPlayerEntity player = baritone.getPlayerContext().player();
+        int entityId = player.getEntityId();
+        logDirect(String.format("Entity id: %d", entityId));
 
-        if (player != null && playerEntityIn.getUniqueID().equals(player.getUniqueID())) {
-            logDirect(String.format("IDs match? -> \u00A7a%s ==? %s", playerEntityIn.getUniqueID().toString(),
-                    player.getUniqueID().toString()));
-            // baritone.getPlayerContext().playerController().
-            // open minigame lobby selection menu
-            if (player.inventory != null) {
-                int item = player.inventory.currentItem;
-                player.inventory.changeCurrentItem(item > 0 ? -item : 0);
-                logDirect(String.format("Item slot: %d", player.inventory.currentItem));
-                logDirect("\u00A7eIDs \u00A7u\u00A7amatched\u00A7r\u00A7e, in first if statement");
-                if (player.inventory.currentItem == 0) {
-                    Hand hand = playerEntityIn.getActiveHand();
-                    // KeyBinding.onTick(mc.gameSettings.keyBindUseItem.getDefault());
+        Entity entity = player.world.getEntityByID(entityId);
+        PlayerEntity playerEntity = (PlayerEntity) entity;
+        ActionResultType type = playerEntity.interactOn(playerEntity, playerEntity.getActiveHand());
+        if (type.SUCCESS)
+            if (player != null && playerEntityIn.getUniqueID().equals(player.getUniqueID())) {
+                logDirect(String.format("IDs match? -> \u00A7a%s ==? %s", playerEntityIn.getUniqueID().toString(),
+                        player.getUniqueID().toString()));
+                // baritone.getPlayerContext().playerController().
+                // open minigame lobby selection menu
+                if (player.inventory != null) {
+                    int item = player.inventory.currentItem;
+                    player.inventory.changeCurrentItem(item > 0 ? -item : 0);
+                    logDirect(String.format("Item slot: %d", player.inventory.currentItem));
+                    logDirect("\u00A7eIDs \u00A7u\u00A7amatched\u00A7r\u00A7e, in first if statement");
+                    if (player.inventory.currentItem == 0) {
+                        Hand hand = playerEntityIn.getActiveHand();
+                        // KeyBinding.onTick(mc.gameSettings.keyBindUseItem.getDefault());
 
-                    // FOR REFERENCE LATER WHEN CLICKING INVENTORY
-                    // baritone.getPlayerContext().playerController().windowClick(windowId, slotId,
-                    // mouseButton, type, player)
-                    baritone.getInputOverrideHandler().clearAllKeys();
-                    baritone.getInputOverrideHandler().setInputForceState(Input.CLICK_RIGHT, true);
-                    logDirect("After input state forced to click");
+                        // FOR REFERENCE LATER WHEN CLICKING INVENTORY
+                        // baritone.getPlayerContext().playerController().windowClick(windowId, slotId,
+                        // mouseButton, type, player)
+                        baritone.getInputOverrideHandler().clearAllKeys();
+                        baritone.getInputOverrideHandler().setInputForceState(Input.CLICK_RIGHT, true);
+                        logDirect("After input state forced to click");
 
-                    // logDirect("\u00A7aSuccessfully right clicked!");
-                    // logDirect("Current item was \u00A7b\u00A7l0, \u00A7r\u00A77in nested if
-                    // statement");
+                        // logDirect("\u00A7aSuccessfully right clicked!");
+                        // logDirect("Current item was \u00A7b\u00A7l0, \u00A7r\u00A77in nested if
+                        // statement");
 
-                    // logDirect(String.format("The container %s null.",
-                    // player.openContainer != null ? "\u00A7a\u00A7uwas not" :
-                    // "\u00A7c\u00A7uwas"));
+                        // logDirect(String.format("The container %s null.",
+                        // player.openContainer != null ? "\u00A7a\u00A7uwas not" :
+                        // "\u00A7c\u00A7uwas"));
 
-                    // logDirect(
-                    // String.format("\u00A77The container was of type: \u00A7e\u00A7l%s",
-                    // player.openContainer));
+                        // logDirect(
+                        // String.format("\u00A77The container was of type: \u00A7e\u00A7l%s",
+                        // player.openContainer));
 
-                    // if (player.openContainer instanceof ChestContainer) {
-                    // ChestContainer gui = (ChestContainer) player.openContainer;
-                    // logDirect("Trying to click on bed");
-                    // logDirect("\u00A77Container type:\u00A7e" + gui.getType().toString());
-                    // gui.slotClick(13, 0, ClickType.PICKUP, player);
-                    // }
+                        // if (player.openContainer instanceof ChestContainer) {
+                        // ChestContainer gui = (ChestContainer) player.openContainer;
+                        // logDirect("Trying to click on bed");
+                        // logDirect("\u00A77Container type:\u00A7e" + gui.getType().toString());
+                        // gui.slotClick(13, 0, ClickType.PICKUP, player);
+                        // }
 
+                        // }
+                    }
+                    *//*
+                     * } else { Thread.sleep(500); run(); }
+                     *//*
+                    *//*
+                     * } catch (Exception e) { }
+                     *//*
                     // }
                 }
-                /*
-                 * } else { Thread.sleep(500); run(); }
-                 */
-                /*
-                 * } catch (Exception e) { }
-                 */
-                // }
             }
-        }
-    }
+    }*/
 }
 // executor.execute(new MoveTask());
